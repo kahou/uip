e6946041618ad8a7f8f40a87de145335aa0cb8c9
Juppeduppe <jesper-jesper@hotmail.com>
2014-02-04 14:18:01 +0100

    Implemented the basic layout with tabs and TaskList

---
+++ MainSrc/layout/EditTask.java
@@ -0,0 +1,12 @@
+package layout;
+import javax.swing.JFrame;
+
+
+public class EditTask {
+	public EditTask(){
+		JFrame popUp = new JFrame();
+		popUp.setSize(900, 700);
+		popUp.setVisible(true);
+	}
+	
+}
