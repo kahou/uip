f6a6385ef08769a52f7d62ab1fc05506947fcc6f
Kahou <c_kahou@hotmail.com>
2014-02-21 00:13:31 +0100

    implement multi languages

--- src/userinterface/MainLayout.java
+++ src/userinterface/MainLayout.java
@@ -1,208 +1,234 @@
 package userinterface;
 
-import java.awt.*;
+import java.awt.BorderLayout;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.FileOutputStream;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
-import javax.swing.border.LineBorder;
 
 public class MainLayout extends JFrame {
-    int height = 0;
-    int width = 0;
-    int inlineLeft = 0;
-    int inlineTop = 0;
-    JFrame window;
+	int height = 0;
+	int width = 0;
+	int inlineLeft = 0;
+	int inlineTop = 0;
+	JFrame window;
+	Internationlization internationlization;
+	int num = 0;
+	ResourceBundle messages;
 
+	String language = "en";
+	String country = "US";
 
-    public static void main(String[] args){
-        SwingUtilities.invokeLater(new Runnable() {
+	public static void main(String[] args) {
 
-            public void run() {
-                MainLayout ex = new MainLayout();
+		SwingUtilities.invokeLater(new Runnable() {
 
-            }
-        });
-    }
+			public void run() {
+				MainLayout ex = new MainLayout();
 
-    /*
-     *
-     * Getters
-     *
-     */
+			}
+		});
+	}
 
-    public int getHeight(){
-        return this.height;
-    }
-    public int getWidth(){
-        return this.width;
-    }
-    public int getInlineTop(){
-        return this.inlineTop;
-    }
-    public int getInlineLeft(){
-        return this.inlineLeft;
-    }
+	/*
+	 * 
+	 * Getters
+	 */
 
-    /*
-     *
-     * Setters
-     *
-     */
-    public void setNewSize(int height,int width){
-        this.height = height;
-        this.width = width;
-    }
-    public void setNewInline(int inlineLeft, int inlineTop){
-        this.inlineLeft = inlineLeft;
-        this.inlineTop = inlineTop;
-    }
+	public int getHeight() {
+		return this.height;
+	}
 
-    //Loads all the properties from the properties file
-    private void LoadConfig(){
+	public int getWidth() {
+		return this.width;
+	}
 
-        File configFile = new File("src/config.properties");
+	public int getInlineTop() {
+		return this.inlineTop;
+	}
 
-        try {
-            FileReader reader = new FileReader(configFile);
-            Properties props = new Properties();
-            props.load(reader);
-            this.height = Integer.parseInt(props.getProperty("height"));
-            this.width = Integer.parseInt(props.getProperty("width"));
-            this.inlineLeft = Integer.parseInt(props.getProperty("inlineLeft"));
-            this.inlineTop = Integer.parseInt(props.getProperty("inlineTop"));
-            reader.close();
-        } catch (FileNotFoundException ex) {
-            // file does not exist
-        } catch (IOException ex) {
-            // I/O error
-        }
-    }
+	public int getInlineLeft() {
+		return this.inlineLeft;
+	}
 
+	/*
+	 * 
+	 * Setters
+	 */
+	public void setNewSize(int height, int width) {
+		this.height = height;
+		this.width = width;
+	}
 
-    //Saves the current position and size of the window
-    private void SaveConfig(){
-        File configFile = new File("src/config.properties");
-        try {
-            FileOutputStream out = new FileOutputStream(configFile);
-            Properties props = new Properties();
-            props.setProperty("height", Integer.toString(getHeight()));
-            props.setProperty("width", Integer.toString(getWidth()));
-            props.setProperty("inlineTop", Integer.toString(getInlineTop()));
-            props.setProperty("inlineLeft", Integer.toString(getInlineLeft()));
-            props.store(out,"");
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+	public void setNewInline(int inlineLeft, int inlineTop) {
+		this.inlineLeft = inlineLeft;
+		this.inlineTop = inlineTop;
+	}
 
+	// Loads all the properties from the properties file
+	private void LoadConfig() {
 
-    // Constructor
-    public MainLayout(){
+		File configFile = new File("src/config.properties");
 
-        //load all configs from property file
-        this.LoadConfig();
+		try {
+			FileReader reader = new FileReader(configFile);
+			Properties props = new Properties();
+			props.load(reader);
+			this.height = Integer.parseInt(props.getProperty("height"));
+			this.width = Integer.parseInt(props.getProperty("width"));
+			this.inlineLeft = Integer.parseInt(props.getProperty("inlineLeft"));
+			this.inlineTop = Integer.parseInt(props.getProperty("inlineTop"));
+			reader.close();
+		} catch (FileNotFoundException ex) {
+			// file does not exist
+		} catch (IOException ex) {
+			// I/O error
+		}
+	}
 
+	// Saves the current position and size of the window
+	private void SaveConfig() {
+		File configFile = new File("src/config.properties");
+		try {
+			FileOutputStream out = new FileOutputStream(configFile);
+			Properties props = new Properties();
+			props.setProperty("height", Integer.toString(getHeight()));
+			props.setProperty("width", Integer.toString(getWidth()));
+			props.setProperty("inlineTop", Integer.toString(getInlineTop()));
+			props.setProperty("inlineLeft", Integer.toString(getInlineLeft()));
+			props.store(out, "");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-        window = new JFrame();
-        window.setLocation(this.inlineTop, this.inlineLeft);
-        window.setSize(this.width,this.height);
+	// Constructor
+	public MainLayout() {
 
+		// load all configs from property file
+		this.LoadConfig();
 
+		Internationlization internationlization = new Internationlization();
+		internationlization.setLanguage(language);
+		internationlization.setCountry(country);
+		Internationlization internationlization2 = new Internationlization();
 
-        JPanel mainPane = new JPanel();
-        JMenuBar menubar = new JMenuBar();
-        JMenu file = new JMenu("File");
-        JMenu file2 = new JMenu("Edit");
-        JMenu file3 = new JMenu("Window");
-        menubar.add(file);
-        menubar.add(file2);
-        menubar.add(file3);
-        setJMenuBar(menubar);
+		messages = internationlization2.getInternationlizationBundle();
 
-        mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.LINE_AXIS));
+		window = new JFrame();
+		window.setLocation(this.inlineTop, this.inlineLeft);
+		window.setSize(this.width, this.height);
 
-        TaskListView tasklist1 = new TaskListView();
+		JPanel mainPane = new JPanel();
+		JMenuBar menubar = new JMenuBar();
+		JMenu file = new JMenu(messages.getString("File"));
+		JMenu file2 = new JMenu(messages.getString("Edit"));
+		JMenu file3 = new JMenu(messages.getString("Window"));
 
-        //add TaskListView and calendar here
-        mainPane.add(tasklist1);
-        mainPane.add(new CalendarView());
+		menubar.add(file);
+		menubar.add(file2);
+		menubar.add(file3);
 
-        JTabbedPane tabbedPane = new JTabbedPane();
-        tabbedPane.addTab("Calendar", mainPane);
+		setJMenuBar(menubar);
 
-        //Create the statusbar
-        /*
-        JLabel statusbar = new JLabel("Statusbar");
-        statusbar.setPreferredSize(new Dimension(-1, 22));
-        statusbar.setBorder(LineBorder.createGrayLineBorder());
-        */
-        window.add(menubar, BorderLayout.NORTH);
-        window.add(tabbedPane);
-        //window.add(statusbar, BorderLayout.SOUTH);
+		mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.LINE_AXIS));
 
+		TaskListView tasklist1 = new TaskListView();
 
+		// add TaskListView and calendar here
+		mainPane.add(tasklist1);
+		mainPane.add(new CalendarView());
 
-        window.setTitle("BorderLayout");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
+		/*
+		 * JButton changeLanguage = new JButton("Change Language");
+		 * changeLanguage.addActionListener(this); mainPane.add(changeLanguage);
+		 */
 
-        window.addComponentListener(new ComponentListener(){
-            @Override
-            public void componentResized(ComponentEvent e) {
-                setNewSize(window.getBounds().height,window.getBounds().width);
-            }
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.addTab(messages.getString("calendar"), mainPane);
 
-            @Override
-            public void componentHidden(ComponentEvent arg0) {
-                // TODO Auto-generated method stub
+		// Create the statusbar
+		/*
+		 * JLabel statusbar = new JLabel("Statusbar");
+		 * statusbar.setPreferredSize(new Dimension(-1, 22));
+		 * statusbar.setBorder(LineBorder.createGrayLineBorder());
+		 */
+		window.add(menubar, BorderLayout.NORTH);
+		window.add(tabbedPane);
+		// window.add(statusbar, BorderLayout.SOUTH);
 
-            }
+		window.setTitle("BorderLayout");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 
-            @Override
-            public void componentMoved(ComponentEvent arg0) {
-                //changes the inline varialbes on change
-                setNewInline(window.getBounds().x, window.getBounds().y);
-                System.out.println(window.getBounds().x + "," + window.getBounds().y);
+		window.addComponentListener(new ComponentListener() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				setNewSize(window.getBounds().height, window.getBounds().width);
+			}
 
-            }
+			@Override
+			public void componentHidden(ComponentEvent arg0) {
+				// TODO Auto-generated method stub
 
-            @Override
-            public void componentShown(ComponentEvent arg0) {
-                // TODO Auto-generated method stub
+			}
 
-            }
-        });
+			@Override
+			public void componentMoved(ComponentEvent arg0) {
+				// changes the inline varialbes on change
+				setNewInline(window.getBounds().x, window.getBounds().y);
+				System.out.println(window.getBounds().x + ","
+						+ window.getBounds().y);
 
+			}
 
-        window.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent evt) {
-                SaveConfig();
-            }
-        });
+			@Override
+			public void componentShown(ComponentEvent arg0) {
+				// TODO Auto-generated method stub
 
+			}
+		});
 
-    }
+		window.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				SaveConfig();
+			}
+		});
+
+	}
+
+	/*
+	 * @Override public void actionPerformed(ActionEvent arg0) {
+	 * 
+	 * num++; if (num > 2) { num = 0; } String[][] langArray = { { "en", "US" },
+	 * { "sv", "SE" }, { "zh", "CN" }, }; System.out.print(langArray[num][0] +
+	 * langArray[num][1]);
+	 * 
+	 * language = langArray[num][0]; country = langArray[num][1];
+	 * internationlization = new Internationlization();
+	 * 
+	 * messages = internationlization.getInternationlizationBundle();
+	 * 
+	 * }
+	 */
 
 }
--- src/userinterface/TaskListView.java
+++ src/userinterface/TaskListView.java
@@ -1,128 +1,148 @@
 package userinterface;
 
-import controller.MainController;
-import storage.Task;
-import storage.TaskList;
-
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
 
-import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+import storage.Task;
+import storage.TaskList;
+import controller.MainController;
 
 /**
- * A view that manages everything related to creating and listing Tasks in the sidebar.
+ * A view that manages everything related to creating and listing Tasks in the
+ * sidebar.
  */
-public class TaskListView extends JPanel implements ActionListener{
-    /**
-     * don't know why this row is needed.
-     */
-    private static final long serialVersionUID = 1L;
+public class TaskListView extends JPanel implements ActionListener {
+	/**
+	 * don't know why this row is needed.
+	 */
+	private static final long serialVersionUID = 1L;
 
-    private JPanel taskListView;
-    private JPanel taskMenu;
-    private MainController controller;
+	private JPanel taskListView;
+	private JPanel taskMenu;
+	private MainController controller;
 
-    public TaskListView(){
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	ResourceBundle messages;
 
-        /**
-         * Create and add a TaskList to the pane.
-         */
-        taskMenu = this.setupTaskList();
-        this.add(taskMenu);
+	public TaskListView() {
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        /**
-         * Configuring and creating taskListView.
-         * Configuring and creating a scrolling panel that will
-         * house the list of tasks.
-         */
-        taskListView = new JPanel();
-        taskListView.setLayout(new BoxLayout(this.taskListView, BoxLayout.Y_AXIS));
-        this.addTasks();
+		Internationlization internationlization = new Internationlization();
+		messages = internationlization.getInternationlizationBundle();
 
-        JScrollPane scrollPane = new JScrollPane(taskListView,
-                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
-                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		/**
+		 * Create and add a TaskList to the pane.
+		 */
+		taskMenu = this.setupTaskList();
+		this.add(taskMenu);
 
-        this.add(scrollPane);
-    }
+		/**
+		 * Configuring and creating taskListView. Configuring and creating a
+		 * scrolling panel that will house the list of tasks.
+		 */
+		taskListView = new JPanel();
+		taskListView.setLayout(new BoxLayout(this.taskListView,
+				BoxLayout.Y_AXIS));
+		this.addTasks();
 
-    /**
-     * Generates a panel with buttons for creating and scrollbox for sorting tasks
-     * @return A panel with buttons, labels and scrollboxes.
-     */
-    private JPanel setupTaskList() {
-        JPanel tempPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.BOTH;
+		JScrollPane scrollPane = new JScrollPane(taskListView,
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-        /**
-         * Create and set constraints for the "New task" button.
-         * Populate the task window with the current taskListView.
-         * Add the button to the pane.
-         */
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.ipady = 20;      //make this component tall
-        c.weightx = 1;
-        JButton newTask = new JButton("New Task");
-        newTask.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                AddTaskView et = new AddTaskView();
-            }
-        });
+		this.add(scrollPane);
+	}
 
-        tempPanel.add(newTask, c);
+	/**
+	 * Generates a panel with buttons for creating and scrollbox for sorting
+	 * tasks
+	 * 
+	 * @return A panel with buttons, labels and scrollboxes.
+	 */
+	private JPanel setupTaskList() {
+		JPanel tempPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
 
-        /**
-         * Create and set the constraints for the "Sort by" label.
-         * Add the label to the pane.
-         */
-        c.gridwidth = 2;
-        c.gridx = 0;
-        c.gridy = 1;
-        JLabel sortLabel = new JLabel("Sort by:", JLabel.CENTER);
-        tempPanel.add(sortLabel,c);
+		/**
+		 * Create and set constraints for the "New task" button. Populate the
+		 * task window with the current taskListView. Add the button to the
+		 * pane.
+		 */
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.ipady = 20; // make this component tall
+		c.weightx = 1;
+		JButton newTask = new JButton(messages.getString("newtask"));
+		newTask.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AddTaskView et = new AddTaskView();
+			}
+		});
 
+		tempPanel.add(newTask, c);
 
-        /**
-         * Create the sort values drop-down menu and set the constraints for it.
-         * Add the label to the pane..
-         */
-        c.gridx = 2;
-        c.gridy = 1;
-        String[] sortValues = {"Starred","Priority","Color"};
-        tempPanel.add(new JComboBox(sortValues),c);
+		/**
+		 * Create and set the constraints for the "Sort by" label. Add the label
+		 * to the pane.
+		 */
+		c.gridwidth = 2;
+		c.gridx = 0;
+		c.gridy = 1;
+		JLabel sortLabel = new JLabel(messages.getString("sortby"),
+				JLabel.CENTER);
+		tempPanel.add(sortLabel, c);
 
-        c.ipady = 10;
-        c.gridx = 0;
-        c.gridy = 2;
-        c.gridwidth = GridBagConstraints.REMAINDER;
+		/**
+		 * Create the sort values drop-down menu and set the constraints for it.
+		 * Add the label to the pane..
+		 */
+		c.gridx = 2;
+		c.gridy = 1;
+		String[] sortValues = { messages.getString("starred"),
+				messages.getString("priority"), messages.getString("color") };
+		tempPanel.add(new JComboBox(sortValues), c);
 
-        JLabel tasksLabel = new JLabel("Tasks");
-        Font font;
-        font = new Font(tasksLabel.getFont().getFontName(), Font.BOLD, tasksLabel.getFont().getSize());
-        tasksLabel.setFont(font);
-        tempPanel.add(tasksLabel, c);
+		c.ipady = 10;
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridwidth = GridBagConstraints.REMAINDER;
 
-        return tempPanel;
-    }
+		JLabel tasksLabel = new JLabel(messages.getString("tasks"));
+		Font font;
+		font = new Font(tasksLabel.getFont().getFontName(), Font.BOLD,
+				tasksLabel.getFont().getSize());
+		tasksLabel.setFont(font);
+		tempPanel.add(tasksLabel, c);
 
-    private void addTasks() {
-        TaskList taskList = TaskList.getInstance();
+		return tempPanel;
+	}
 
-        for (Task tempTask : taskList.getTaskList()) {
-            TaskView newTaskView = new TaskView(tempTask);
-            taskListView.add(newTaskView);
-            taskListView.add(Box.createRigidArea(new Dimension(0, 5)));
-        }
-    }
+	private void addTasks() {
+		TaskList taskList = TaskList.getInstance();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+		for (Task tempTask : taskList.getTaskList()) {
+			TaskView newTaskView = new TaskView(tempTask);
+			taskListView.add(newTaskView);
+			taskListView.add(Box.createRigidArea(new Dimension(0, 5)));
+		}
+	}
 
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 
+	}
 
 }
