897b0d940cbfe8d242d48ad8cc2e453a0477467d
DaZhang <zhangda137@hotmail.com>
2014-03-06 16:11:14 +0100

    mainlayout

--- src/userinterface/MainLayout.java
+++ src/userinterface/MainLayout.java
@@ -1,82 +1,141 @@
 package userinterface;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.border.LineBorder;
-
-import storage.Task;
-import storage.TaskList;
-
 import javax.swing.plaf.metal.*;
 import javax.swing.plaf.basic.*;
 import javax.swing.plaf.multi.*;
 import javax.swing.plaf.synth.*;
 
+import controller.MainController;
+/**
+ * 
+ * MainLayout is the main gui class that contains everything the gui has.
+ * 
+ *
+ */
 public class MainLayout extends JFrame {
+
+	JFrame window;
+	Internationlization internationlization;
+	int num = 0;
+	ResourceBundle messages;
+	MainController controller = MainController.getInstance();
 
 	final static String LOOKANDFEEL = "Metal";
 	final static String THEME = "Default";
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
 
-			public void run() {
-				MainLayout ex = new MainLayout();
-			}
-		});
-	}
 
+	// Constructor
 	public MainLayout() {
+
+		// load all configs from property file
+		controller.LoadConfig();
+
+		Internationlization internationlization = new Internationlization();
+		internationlization.setLanguage(controller.getLanguage());
+		internationlization.setCountry(controller.getCountry());
+		Internationlization internationlization2 = new Internationlization();
+
+		messages = internationlization2.getInternationlizationBundle();
 		
 		//Set the look and feel.
         initLookAndFeel();
         //Make sure we have nice window decorations.
         JFrame.setDefaultLookAndFeelDecorated(true);
+		
 
-		JFrame window = new JFrame();
-		window.setSize(900, 700);
+		window = this;
+		this.setLocation(controller.getInlineLeft(),controller.getInlineTop());
+		this.setSize(controller.getWidth(), controller.getHeight());
 
-		JTabbedPane tabbedPane = new JTabbedPane();
 
+		//Menu initialization.
 		JPanel mainPane = new JPanel();
-		
-		
+		JMenuBar menubar = new MainMenu(messages);
+
+
+		this.setJMenuBar(menubar);
+
 		mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.LINE_AXIS));
 
-		
-		window.add(menubar, BorderLayout.NORTH);
-
 		TaskListView tasklist1 = new TaskListView();
-		// add TaskListView here
 
+		// add TaskListView and calendar here
 		mainPane.add(tasklist1);
-
-		// add calendar here
-		// mainPane.add(new JTextArea());
 		mainPane.add(new CalendarView());
 
-		tabbedPane.addTab("Calendar", mainPane);
-		window.add(tabbedPane);
-		JLabel statusbar = new JLabel("Statusbar");
-		statusbar.setPreferredSize(new Dimension(-1, 22));
-		statusbar.setBorder(LineBorder.createGrayLineBorder());
-		window.add(statusbar, BorderLayout.SOUTH);
+		/*
+		 * JButton changeLanguage = new JButton("Change Language");
+		 * changeLanguage.addActionListener(this); mainPane.add(changeLanguage);
+		 */
 
-		window.setTitle("BorderLayout");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.addTab(messages.getString("calendar"), mainPane);
 
+		this.add(menubar, BorderLayout.NORTH);
+		this.add(tabbedPane);
+		this.setTitle(messages.getString("calendar"));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+		this.addComponentListener(new ComponentListener() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				controller.setNewSize(window.getBounds().height, window.getBounds().width);
+			}
+
+			@Override
+			public void componentHidden(ComponentEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void componentMoved(ComponentEvent arg0) {
+				// changes the inline varialbes on change
+				controller.setNewInline(window.getBounds().x, window.getBounds().y);
+
+			}
+
+			@Override
+			public void componentShown(ComponentEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		window.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				controller.SaveConfig();
+			}
+		});
+		
 	}
 
 	private static void initLookAndFeel() {
@@ -134,5 +193,8 @@ public class MainLayout extends JFrame {
 			}
 		}
 	}
+
+
+
 
 }
