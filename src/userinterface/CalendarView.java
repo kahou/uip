d1c83951b35b810bbd3b6e766c411f7b074fc265
rbare1 <rbare1@ycp.edu>
2014-03-06 17:16:57 +0100

    Fixed the dates to actually work Table now updates based on the month/year Table displays tasks

--- src/userinterface/CalendarView.java
+++ src/userinterface/CalendarView.java
@@ -4,43 +4,74 @@ package userinterface;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.ResourceBundle;
 
 import javax.swing.*;
 import javax.swing.table.*;
 
+import storage.TaskList;
+
 
 public class CalendarView extends JPanel implements ActionListener {
+	Calendar calDate = GregorianCalendar.getInstance(); //the current date displayed on the calendar
+	Calendar currentDate = GregorianCalendar.getInstance();	
 	
-	public CalendarView() {
-		String months[] = {"January", "February","March", "April","May","June","July","August","September","October","November","December"};
-
+	JPanel panelTable = new JPanel();
+	DefaultTableModel tableModel = new DefaultTableModel();
+	int firstDay;
+	HashMap<Integer, Integer> monthTasks = new HashMap<Integer, Integer>();
+	
+	
+	Container pane = this;
+	String months[] = {"January", "February","March", "April","May","June","July","August","September","October","November","December"};
+	JLabel labelMonth = new JLabel(months[calDate.get(Calendar.MONTH)]  + " " + calDate.get(Calendar.YEAR));
+	
+	
+	public CalendarView(ResourceBundle messages) {
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        //set starting calendar month
+		calDate.set(currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), 1); //sets calDate to the first day of the current month	
 		  //Declare Frame
 		  Container pane = this;
 		  
 		  //Declare Panels
 		  JPanel panelLblCurrentDate = new JPanel();
 		  JPanel panelLblMonth = new JPanel();
-		  JPanel panelTable = new JPanel();
 		  
 		  //Declare Labels
-		  JLabel labelCurrentDate = new JLabel(String.valueOf(Calendar.DATE-1) + ", " + months[Calendar.MONTH-1] + ", " + String.valueOf(Calendar.YEAR));
-		  JLabel labelMonth = new JLabel(months[Calendar.MONTH]); //should not use current month, should be changeable
+		  JLabel labelCurrentDate = new JLabel(String.valueOf(currentDate.get(Calendar.DATE)) + ", " + months[currentDate.get(Calendar.MONTH)] + ", " + String.valueOf(currentDate.get(Calendar.YEAR)));
 		  
 		  //Current date label and panel
 		  panelLblCurrentDate.setBorder(BorderFactory.createLineBorder(Color.black));
 		  panelLblCurrentDate.add(labelCurrentDate);
 		  pane.add(panelLblCurrentDate);
 		   
-		  //Backwards icon/button
+		  //Backwards icon/button to change current month displayed
 		  java.net.URL backArrowURL = getClass().getResource("/resources/backArrow.gif");
 		    if (backArrowURL != null) {
 		    	 JButton buttonBackArrow = new JButton(new ImageIcon(backArrowURL));
 				  panelLblMonth.add(buttonBackArrow);
 				  buttonBackArrow.setBorder(BorderFactory.createEmptyBorder());
 				  buttonBackArrow.setContentAreaFilled(false);
+				  
+				  buttonBackArrow.addActionListener(new ActionListener() {	
+			            public void actionPerformed(ActionEvent e) {
+			            	int tempMonth = calDate.get(Calendar.MONTH);
+			            	if(tempMonth == 0){
+			            		calDate.set(Calendar.MONTH, 11); 
+			            		calDate.set(Calendar.YEAR, calDate.get(Calendar.YEAR) - 1);
+			            	}
+			            	else{
+			            		calDate.set(Calendar.MONTH,( tempMonth - 1));   
+			            	}
+			                tableUpdate();
+			                labelMonth.setText(months[calDate.get(Calendar.MONTH)] + " " + calDate.get(Calendar.YEAR));	//gets the month, must use get method to get correct value
+			            }
+			        });
 		    }
 		 
 		  
@@ -49,18 +80,42 @@ public class CalendarView extends JPanel implements ActionListener {
 		  panelLblMonth.add(labelMonth);
 		  pane.add(panelLblMonth);
 		  
-		  //Forwards icon/button
+		  //Forwards icon/button to change current month displayed
 		  java.net.URL forwardArrowURL = getClass().getResource("/resources/forwardArrow.gif");
 		    if (forwardArrowURL != null) {
 		    	 JButton buttonForwardArrow = new JButton(new ImageIcon(forwardArrowURL));
 				  panelLblMonth.add(buttonForwardArrow);
 				  buttonForwardArrow.setBorder(BorderFactory.createEmptyBorder());
 				  buttonForwardArrow.setContentAreaFilled(false);
+				  
+				  buttonForwardArrow.addActionListener(new ActionListener() {	
+					  															
+			            public void actionPerformed(ActionEvent e) {
+			            	int tempMonth = calDate.get(Calendar.MONTH);
+			            	if(tempMonth == 11){
+			            		calDate.set(Calendar.MONTH, 0); 
+			            		calDate.set(Calendar.YEAR, calDate.get(Calendar.YEAR) + 1);
+			            	}
+			            	else{
+			            		calDate.set(Calendar.MONTH, tempMonth + 1);  
+			            	}
+			                tableUpdate();
+			                labelMonth.setText(months[calDate.get(Calendar.MONTH)] + " " + calDate.get(Calendar.YEAR));	//gets the month, must use get method to get correct value
+			            }
+			        });
 		    }
 		  
-		  
+		    tableSetup();
+		
+
+	}
+
+	/**
+	 * Initial setup of the table for the current month
+	 */
+	public void tableSetup(){
 		  //Table set up
-		  DefaultTableModel tableModel = new DefaultTableModel();
+		  
 		  JTable calTable = new JTable(tableModel);
 		  String[] columnNames = {"SUN", "MON", "TUE", "WED", "THU", "FRI","SAT"};
 		  
@@ -68,23 +123,20 @@ public class CalendarView extends JPanel implements ActionListener {
 		  for(int i = 0; i < 7; i++){
 		   tableModel.addColumn(columnNames[i]);
 		  }
-		  tableModel.setRowCount(5);
+		  tableModel.setRowCount(6);
 		  calTable.setRowHeight(450/tableModel.getRowCount());
 		  JTableHeader tableHeader = calTable.getTableHeader(); //display days of the week
 		  tableHeader.setReorderingAllowed(false);
 		  tableHeader.setResizingAllowed(false);
 		  
-		  
-		  Calendar calDate = GregorianCalendar.getInstance();
-		  calDate.set(Calendar.YEAR, Calendar.MONTH, 0); //sets calDate to the first day of the current month
-		              //should use month at the top of the calendar in the future
-		  int firstDay = calDate.DAY_OF_WEEK;    
+		  firstDay = calDate.get(Calendar.DAY_OF_WEEK);
+
 		  
 		  int dateSet = 1;
 		  int j =firstDay - 1; //puts the first day of the month on the proper day of the week
-		  for(int i = 0; i < 5; i++){
+		  for(int i = 0; i < 6; i++){
 		   for(; j < 7; j++){
-		    if(dateSet <= calDate.getActualMaximum(calDate.DAY_OF_MONTH)){
+		    if(dateSet <= calDate.getActualMaximum(Calendar.DAY_OF_MONTH)){
 		     tableModel.setValueAt(dateSet, i, j);
 		     dateSet++;
 		    }
@@ -96,9 +148,67 @@ public class CalendarView extends JPanel implements ActionListener {
 		  panelTable.add(tableHeader);
 		  panelTable.add(calTable);
 		  pane.add(panelTable);
-
 	}
+	
+	/**
+	 * Changes the days and tasks on the calendar to match current month and year
+	 */
+	public void tableUpdate(){
+		tableModel.setRowCount(0);	//clear the table
+		tableModel.setRowCount(6); //add the rows back in
+		
+		firstDay = calDate.get(Calendar.DAY_OF_WEEK); 
+		getTasks();
+		  int dateSet = 1;
+		  int j = firstDay - 1; //puts the first day of the month on the proper day of the week
+		  for(int i = 0; i < 6; i++){
+		   for(; j < 7; j++){
+		    if(dateSet <= calDate.getActualMaximum(Calendar.DAY_OF_MONTH)){
+		    	if(monthTasks.containsKey(dateSet)){
+		    		tableModel.setValueAt(dateSet + "   " + monthTasks.get(dateSet) + " Tasks", i, j);
 
+		    	}
+		    	else{
+		    		tableModel.setValueAt(dateSet, i, j);
+		    	}
+		     dateSet++;
+		    }
+		   }
+		   j=0;
+		  }
+		tableModel.fireTableDataChanged();		
+	}
+	
+	/**
+	 * Retrieves the lists of tasks
+	 * Adds tasks from the current month and year in a map 
+	 */
+	public void getTasks(){
+		TaskList taskList = TaskList.getInstance();
+		
+		monthTasks.clear();
+		if(taskList.getListSize() > 0){
+			for(int i = 0; i < taskList.getListSize(); i++){
+				if((taskList.getTaskByIndex(i).getStartTime() != null)){
+					if(taskList.getTaskByIndex(i).getStartTime().getMonthOfYear() == calDate.get(Calendar.MONTH) + 1){	//compares the month of the task to the current month of the calendar
+						//check year																					//Joda Time months start at 1 but Calendar months start at 0, thus the + 1
+						if(taskList.getTaskByIndex(i).getStartTime().getYear() == calDate.get(Calendar.YEAR)){
+							int tempNum = 0;
+							if(monthTasks.containsKey(i)){
+								tempNum = monthTasks.get(i) + 1;
+							}
+							else{
+								tempNum = 1;
+							}
+							monthTasks.put(taskList.getTaskByIndex(i).getStartTime().getDayOfMonth(), tempNum);
+						}
+						
+					}
+				}
+			}
+		}
+		
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
